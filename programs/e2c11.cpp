d95a28303a3ccb2cfdf205ad6bc45f746cf0a671
shadow_walker <shadowwalkersb@gmail.com>
2017-09-14 14:37:38 -0500

    e2c11

---
+++ programs/e2c11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int i = 6;
+    int f = 11;
+    
+    auto a = i;
+    
+    cout<<"Hello there!\n"
+            <<a
+        <<endl;
+
+    return 0;
+}
