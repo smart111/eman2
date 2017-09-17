5a22fd73ea50b9500e8c359d15c52786a20d4303
shadow_walker <shadowwalkersb@gmail.com>
2017-09-17 11:07:08 -0500

    add dot.* and libpyCuda.cu

---
+++ libEM/cuda/dot.h
@@ -0,0 +1,5 @@
+#ifndef EMAN_DOT_H
+#define EMAN_DOT_H
+
+
+#endif //EMAN_DOT_H
---
+++ libEM/cuda/libpyCuda.cu
@@ -0,0 +1,9 @@
+#include <boost/python.hpp>
+#include "dot.h"
+
+using namespace boost::python;
+
+
+BOOST_PYTHON_MODULE(libpyCuda)
+        {
+        };
