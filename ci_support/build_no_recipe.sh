6797fb7768c04949bf48e84313749597f6e7e8a1
shadow_walker <shadowwalkersb@gmail.com>
2017-10-06 22:01:17 -0500

    numpy

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -2,13 +2,7 @@
 
 source ci_support/setup_conda.sh
 
-# Following Wiki instructions at
-# http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
-if [ "$(uname -s)" != "Darwin" ];then
-    conda install --yes --quiet eman-deps="*"="np18*" -c cryoem -c defaults -c conda-forge
-else
-    conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
-fi
+conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
 
 # Build and install eman2
 export build_dir=$HOME/build_eman
--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -6,8 +6,4 @@ export CPU_COUNT=2
 
 conda install conda-build=2 -c defaults --yes --quiet
 
-if [ "$(uname -s)" != "Darwin" ];then
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge --numpy 1.8
-else
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge
-fi
+conda build recipes/eman -c cryoem -c defaults -c conda-forge
