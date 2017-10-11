ab9e5258240d2e18d670ab3eff3ca63d16a3cc2b
shadow_walker <shadowwalkersb@gmail.com>
2017-10-11 17:06:39 -0500

    fix data path

--- del/eman_test/test_script.sh
+++ del/eman_test/test_script.sh
@@ -1,6 +1,6 @@
 rm -rf test_00
 mkdir test_00
-e2simmx.py init_ref.hdf init_ptcls.hdf test_00/simmx.hdf -f --saveali --cmp=frc:maxres=25.0 --align=rotate_translate_tree --aligncmp=ccc --force --verbose=-1 --ralign=refine --raligncmp=ccc --parallel=thread:12
+e2simmx.py data/init_ref.hdf data/init_ptcls.hdf test_00/simmx.hdf -f --saveali --cmp=frc:maxres=25.0 --align=rotate_translate_tree --aligncmp=ccc --force --verbose=-1 --ralign=refine --raligncmp=ccc --parallel=thread:12
 e2classify.py test_00/simmx.hdf test_00/classmx.hdf -f --sep 1
 e2classaverage.py --input init_ptcls.hdf --classmx test_00/classmx.hdf --decayedge --storebad --output test_00/classes.hdf --ref init_ref.hdf --iter 1 -f --resultmx test_00/clsresult.hdf --normproc normalize.edgemean --averager mean --keep 1. --cmp frc:maxres=25 --align rotate_translate_tree --aligncmp ccc --ralign refine --raligncmp ccc --parallel thread:12
 e2make3dpar.py --input test_00/classes.hdf --sym d2 --output test_00/threed.hdf --keep 1 --apix 2.55 --pad 208 --mode gauss_5 --threads 12
