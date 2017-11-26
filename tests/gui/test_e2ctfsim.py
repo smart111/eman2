57ccfcded6c3a150c49dd5d745a0f4636b3ff42a
shadow_walker <shadowwalkersb@gmail.com>
2017-11-25 21:27:11 -0600

    e2ctfsim

--- tests/gui/test_e2ctfsim.py
+++ tests/gui/test_e2ctfsim.py
@@ -1,22 +1,6 @@
-import os
-import e2ctfsim
-
-import pytest
-
-
-@pytest.fixture
-def main_form():
-    dirname = 'e2ctfsim'
-    if not os.path.isdir(dirname):
-        os.mkdir(dirname)
-
-    main_form = e2ctfsim.main_loop([])
-    yield main_form
-    main_form.close()
-
-def test_cli(qtbot, main_form, win):
-    win.cycle(qtbot, main_form,'e2ctfsim')
-
-    win.cycle(qtbot, main_form.guiim,'e2ctfsim')
-    win.cycle(qtbot, main_form.guiplot,'e2ctfsim')
+def test_cli(qtbot, win):
+    win = win('e2ctfsim')
+    win.cycle(qtbot, win.main_form)
+    win.cycle(qtbot, win.main_form.guiim)
+    win.cycle(qtbot, win.main_form.guiplot)
 
