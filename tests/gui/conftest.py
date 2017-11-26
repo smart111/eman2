c41eb47f675b9775c4bef61e1bcc35de52b1d086
shadow_walker <shadowwalkersb@gmail.com>
2017-11-25 22:14:04 -0600

    lower sleep times

--- tests/gui/conftest.py
+++ tests/gui/conftest.py
@@ -41,13 +41,13 @@ class Win(object):
         if clickButton:
             qtbot.mouseClick(form, clickButton)
         qtbot.waitForWindowShown(form)
-        qtbot.wait(1000)
+        qtbot.wait(100)
         fname = '%s.png'%os.path.join(self.dir,str(self.counter))
         qpxmap = QPixmap.grabWindow(form.winId())
-        qtbot.wait(1000)
+        qtbot.wait(100)
         qpxmap.save(fname,'png')
         print("Click!: %s"%fname)
-        qtbot.wait(800)
+        qtbot.wait(100)
         self.counter += 1
 
 @pytest.fixture
