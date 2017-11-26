57da5f0dfab62c02486c6cfcb0e0fcd79ab57bf6
shadow_walker <shadowwalkersb@gmail.com>
2017-11-25 21:41:39 -0600

    test_e2display

--- tests/gui/test_e2display.py
+++ tests/gui/test_e2display.py
@@ -1,16 +1,3 @@
-import os
-import e2display
-import pytest
-
-
-@pytest.fixture
-def main_form():
-    dirname = 'e2display'
-    if not os.path.isdir(dirname):
-        os.mkdir(dirname)
-    main_form = e2display.main([])
-    yield main_form
-    main_form.close()
-
-def test_display_file(qtbot, main_form, win):
-    win.cycle(qtbot, main_form,'e2display')
+def test_display_file(qtbot, win):
+    main_form = win('e2display')
+    win.cycle(qtbot, main_form)
