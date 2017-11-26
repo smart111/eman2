9999b39245e4f84d090c34ffb99ea8198554980d
shadow_walker <shadowwalkersb@gmail.com>
2017-11-25 21:58:14 -0600

    e2boxer

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -1,20 +1,8 @@
 import os
 from PyQt4.QtCore import Qt
-import e2boxer
 
-import pytest
-
-@pytest.fixture
-def main_form(curdir):
-    dirname = 'e2boxer'
-    if not os.path.isdir(dirname):
-        os.mkdir(dirname)
-    main_form = e2boxer.main_loop(apix=1, imagenames=["1\t%s"%os.path.join(curdir,'e2boxer/test_box.hdf')])
-    yield main_form
-    main_form.close()
-        
-def test_cli(qtbot, main_form, win):
-    win.cycle(qtbot, main_form,'e2boxer')
-    
-    win.cycle(qtbot, main_form.wimage,'e2boxer', Qt.LeftButton)
-    win.cycle(qtbot, main_form.wparticles,'e2boxer')
+def test_cli(qtbot, win, curdir):
+    win = win('e2boxer', apix=1, imagenames=["1\t%s"%os.path.join(curdir,'e2boxer/test_box.hdf')])
+    win.cycle(qtbot, win.main_form)
+    win.cycle(qtbot, win.main_form.wimage, Qt.LeftButton)
+    win.cycle(qtbot, win.main_form.wparticles)
