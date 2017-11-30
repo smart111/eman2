20649e251c6c8821a4295f0cbc6fcce4464d1c0e
shadow_walker <shadowwalkersb@gmail.com>
2017-11-30 17:50:56 -0600

    cleanup

--- tests/gui/test_e2history.py
+++ tests/gui/test_e2history.py
@@ -1,12 +1,6 @@
-import pytest
 from PyQt4.QtCore import Qt
-import e2history
 
-@pytest.fixture
-def main_form():
-    return e2history.main_loop()
 
-def test_okButton(qtbot,main_form):
-    qtbot.waitExposed(main_form.form)
-    qtbot.wait(1000)
+def test_okButton(qtbot, win):
+    main_form = win('e2history').main_form
     qtbot.mouseClick(main_form.form.layout().itemAt(1).itemAt(1).widget(), Qt.LeftButton)
