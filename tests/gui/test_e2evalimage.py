079f650af1fda14efea4bf678ada0956f894870f
shadow_walker <shadowwalkersb@gmail.com>
2017-11-30 22:08:47 -0600

    cleanup

--- tests/gui/test_e2evalimage.py
+++ tests/gui/test_e2evalimage.py
@@ -1,19 +1,12 @@
 import os
 
 
-@pytest.fixture
-def main_form(curdir):
-    dirname = 'e2evalimage'
-    if not os.path.isdir(dirname):
-        os.mkdir(dirname)
-    main_form = e2evalimage.main_loop([os.path.join(curdir,'e2evalimage/BGal_000232.hdf')])
-    yield main_form
-    main_form.close()
-
-def test_display_file(qtbot, main_form, win):
+def test_display_file(qtbot, win, curdir):
+    win = win('e2evalimage',[os.path.join(curdir,'e2evalimage/BGal_000232.hdf')])
+    main_form = win.main_form
     qtbot.addWidget(main_form)
 
-    win.cycle(qtbot, main_form,'e2evalimage')
-    win.cycle(qtbot, main_form.wplot, 'e2evalimage')
-    win.cycle(qtbot, main_form.wfft, 'e2evalimage')
-    win.cycle(qtbot, main_form.wimage, 'e2evalimage')
+    win.cycle(qtbot, main_form)
+    win.cycle(qtbot, main_form.wplot)
+    win.cycle(qtbot, main_form.wfft)
+    win.cycle(qtbot, main_form.wimage)
