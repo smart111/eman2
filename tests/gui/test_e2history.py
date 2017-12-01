8989724e65696f25ac66ea2741a64955a5d1133c
shadow_walker <shadowwalkersb@gmail.com>
2017-12-01 11:42:12 -0600

    sys_argv, return

--- tests/gui/test_e2history.py
+++ tests/gui/test_e2history.py
@@ -2,7 +2,7 @@ from PyQt4.QtCore import Qt
 
 
 def test_okButton(qtbot, win):
-    win = win('e2history')
+    win = win('e2history',["--gui"])
     main_form = win.main_form
     win.cycle(qtbot, main_form.form)
     qtbot.mouseClick(main_form.form.layout().itemAt(1).itemAt(1).widget(), Qt.LeftButton)
