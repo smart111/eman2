25136f608428b09d04ca0d0f0bbd47ad52009b51
shadow_walker <shadowwalkersb@gmail.com>
2017-12-01 12:21:56 -0600

    sys_argv, return

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -2,7 +2,7 @@ import os
 from PyQt4.QtCore import Qt
 
 def test_cli(qtbot, win, curdir):
-    win = win('e2boxer', apix=1, imagenames=["1\t%s"%os.path.join(curdir,'e2boxer/test_box.hdf')])
+    win = win('e2boxer', ["--gui", "--apix=1", "--no_ctf", os.path.join(curdir,'e2boxer/test_box.hdf')])
     win.cycle(qtbot, win.main_form)
     win.cycle(qtbot, win.main_form.wimage, Qt.LeftButton)
     win.cycle(qtbot, win.main_form.wparticles)
