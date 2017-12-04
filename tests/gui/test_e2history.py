9a17de6bad27c79a79b6b3e4929693b83b0f73ea
shadow_walker <shadowwalkersb@gmail.com>
2017-12-04 14:28:56 -0600

    working

--- tests/gui/test_e2history.py
+++ tests/gui/test_e2history.py
@@ -1,8 +1,8 @@
 from PyQt4.QtCore import Qt
 
 
-def test_okButton(qtbot, win):
-    win = win('e2history',["--gui"])
-    main_form = win.main_form
-    win.cycle(qtbot, main_form.form)
+def test_okButton(main_form, win, qtbot):
+    main_form = main_form('e2history',["--gui"])
+    win = win('e2history')
+    win(main_form.form)
     qtbot.mouseClick(main_form.form.layout().itemAt(1).itemAt(1).widget(), Qt.LeftButton)
