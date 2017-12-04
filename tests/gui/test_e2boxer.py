35a208f9205b5b3c5a2a71009425aec193f7d3ea
shadow_walker <shadowwalkersb@gmail.com>
2017-12-04 14:46:59 -0600

    working

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -1,8 +1,9 @@
-import os
 from PyQt4.QtCore import Qt
+import os
 
-def test_cli(qtbot, win, curdir):
-    win = win('e2boxer', ["--gui", "--apix=1", "--no_ctf", os.path.join(curdir,'e2boxer/test_box.hdf')])
-    win.cycle(qtbot, win.main_form)
-    win.cycle(qtbot, win.main_form.wimage, Qt.LeftButton)
-    win.cycle(qtbot, win.main_form.wparticles)
+def test_display_initial_gui(main_form, win, curdir):
+    main_form = main_form('e2boxer', ["--gui", "--apix=1", "--no_ctf", os.path.join(curdir,'e2boxer/test_box.hdf')])
+    win = win('e2boxer')
+    win(main_form)
+    win(main_form.wimage, Qt.LeftButton)
+    win(main_form.wparticles)
