2c1a698adc90fc4a8d6cba71e7c9dd42da9f8620
shadow_walker <shadowwalkersb@gmail.com>
2017-12-04 14:43:35 -0600

    working

--- tests/gui/test_e2filtertool.py
+++ tests/gui/test_e2filtertool.py
@@ -2,10 +2,11 @@ from PyQt4.QtCore import Qt
 import os
 
 
-def test_mouseClick_altModifier(qtbot, win, curdir):
-    win = win('e2filtertool',['%s'%os.path.join(curdir,'e2display/twod.hdf')])
-    win.cycle(qtbot, win.main_form)
-    win.cycle(qtbot, win.main_form.viewer[0])
-    qtbot.mouseClick(win.main_form.viewer[0], Qt.LeftButton, Qt.AltModifier)
+def test_display_initial_gui(main_form, win, curdir, qtbot):
+    main_form = main_form('e2filtertool',['%s'%os.path.join(curdir,'e2display/twod.hdf')])
+    win = win('e2filtertool')
+    win(main_form)
+    win(main_form.viewer[0])
+    qtbot.mouseClick(main_form.viewer[0], Qt.LeftButton, Qt.AltModifier)
     qtbot.wait(1000)
 # Need a set of filters and filter types to test spinboxes, checkboxes and buttons
