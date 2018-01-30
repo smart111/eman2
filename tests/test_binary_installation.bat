cef142cf47aa5ba03fe5be875a398e97a9965c58
shadow_walker <shadowwalkersb@gmail.com>
2018-01-29 18:54:17 -0600

    run tests [ci skip]

--- tests/test_binary_installation.bat
+++ tests/test_binary_installation.bat
@@ -1,3 +1,4 @@
+set "BASH_EXE=C:\Program Files\Git\bin\bash.exe"
 set INSTALLER_DIR=C:\Users\EMAN\workspace\win-installers
 set INSTALLER_FILE=eman2.win64.exe
 set INSTALLATION_DIR=eman2.win64
@@ -5,4 +6,13 @@ set INSTALLATION_DIR=eman2.win64
 rmdir /q /s %INSTALLER_DIR%\%INSTALLATION_DIR%
 
 start /wait "" %INSTALLER_DIR%\%INSTALLER_FILE% /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%INSTALLER_DIR%\%INSTALLATION_DIR%
+if errorlevel 1 exit 1
+
+call %INSTALLER_DIR%\%INSTALLATION_DIR%\Scripts\activate.bat
+if errorlevel 1 exit 1
+
+call tests\run_tests.bat
+if errorlevel 1 exit 1
+
+"%BASH_EXE%" -c "bash tests/run_prog_tests.sh"
 if errorlevel 1 exit 1
