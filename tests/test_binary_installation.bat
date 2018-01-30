59407d4e88d863c92bfc87e1426053b633c01249
shadow_walker <shadowwalkersb@gmail.com>
2018-01-30 07:03:25 -0600

    tests only [ci skip]

--- tests/test_binary_installation.bat
+++ tests/test_binary_installation.bat
@@ -3,10 +3,10 @@ set INSTALLER_DIR=C:\Users\EMAN\workspace\win-installers
 set INSTALLER_FILE=eman2.win64.exe
 set INSTALLATION_DIR=eman2.win64
 
-rmdir /q /s %INSTALLER_DIR%\%INSTALLATION_DIR%
+::rmdir /q /s %INSTALLER_DIR%\%INSTALLATION_DIR%
 
-start /wait "" %INSTALLER_DIR%\%INSTALLER_FILE% /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%INSTALLER_DIR%\%INSTALLATION_DIR%
-if errorlevel 1 exit 1
+::start /wait "" %INSTALLER_DIR%\%INSTALLER_FILE% /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%INSTALLER_DIR%\%INSTALLATION_DIR%
+::if errorlevel 1 exit 1
 
 call %INSTALLER_DIR%\%INSTALLATION_DIR%\Scripts\activate.bat
 if errorlevel 1 exit 1
