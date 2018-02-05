526bf9e143267da30c8e7e232930dc112dda143f
Zhong Huang <zhonghuang@huang.local>
2018-02-04 23:15:14 -0600

    corrections

--- sparx/bin/sxsort3d_depth.py
+++ sparx/bin/sxsort3d_depth.py
@@ -3812,7 +3812,7 @@ def do_boxes_two_way_comparison_new(nbox, input_box_parti1, input_box_parti2, de
 	score_list = [ ]
 	nclass = 0
 	log_main.add('               Post-matching results.')
-	log_main.add('{:^14} {:^10}  {:^8} {:^15} {:^22}'.format('    Group', '   size',  ' status ',   'reproducibility', 'random reproducibility'))
+	log_main.add('{:^14} {:^10}  {:^8} {:^15} {:^22}  {:^5}'.format('    Group', '   size',  ' status ',   'reproducibility', 'random reproducibility', ' std '))
 	
 	for index_of_any in xrange(len(list_stable)):
 		any = list_stable[index_of_any]
@@ -3828,9 +3828,9 @@ def do_boxes_two_way_comparison_new(nbox, input_box_parti1, input_box_parti2, de
 			maximum_group_size = max(maximum_group_size, len(any))
 			new_list.append(any)
 			nclass +=1
-			log_main.add('{:^14d} {:^10d} {:^8} {:^15.1f} {:^22.1f}'.format(index_of_any, len(any),'accepted', score3, table_stat(clist[index_of_any])[0]))
+			log_main.add('{:^14d} {:^10d} {:^8} {:^15.1f} {:^22.1f} {:^5.1f}'.format(index_of_any, len(any),'accepted', score3, table_stat(clist[index_of_any])[0]), table_stat(clist[index_of_any])[1]))
 		else:
-			log_main.add('{:^14d} {:^10d} {:^8} {:^15.1f} {:^22.1f}'.format(index_of_any, len(any), 'rejected', score3, table_stat(clist[index_of_any])[0]))
+			log_main.add('{:^14d} {:^10d} {:^8} {:^15.1f} {:^22.1f} {:^5.1f}'.format(index_of_any, len(any), 'rejected', score3, table_stat(clist[index_of_any])[0]), table_stat(clist[index_of_any])[1]))
 	
 	if nclass == 0:
 		### redo two way comparison
