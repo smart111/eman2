1b5a375ee2edc66ed935f382569924976aaa9897
Zhong Huang <zhonghuang@huang.local>
2018-02-04 23:11:51 -0600

    corrections

--- sparx/bin/sxsort3d_depth.py
+++ sparx/bin/sxsort3d_depth.py
@@ -1181,22 +1181,22 @@ def do_one_way_anova_scipy(clusters, value_list, name_of_variable="variable", lo
 	mst = sst/float(n1)
 	f_ratio = msa/mse
 	log_main.add('                              ANOVA of %s'%name_of_variable)
-	log_main.add('{:5} {:^12} {:^12} {}'.format('ANOVA', 'F-value',  'p_value'))
-	log_main.add('{:5} {:12.2f} {:12.2f} {}'.format('ANOVA', res[0], res[1]))
+	log_main.add('{:5} {:^12} {:^12} '.format('ANOVA', 'F-value',  'p_value'))
+	log_main.add('{:5} {:12.2f} {:12.2f}'.format('ANOVA', res[0], res[1]))
 	log_main.add(' ')
 
 	log_main.add('ANOVA:  %s mean of all clusters: %f'%(name_of_variable, global_mean/(float(nsamples))))
 	log_main.add('ANOVA:  Group averages')
-	log_main.add('{:5} {:^7} {:^8} {:^12} {:^12} {}'.format('ANOVA', 'GID', 'N',  'mean',   'std'))
+	log_main.add('{:5} {:^7} {:^8} {:^12} {:^12} '.format('ANOVA', 'GID', 'N',  'mean',   'std'))
 	for i in xrange(K):
-		log_main.add('{:5} {:^7d} {:^8d} {:12.2f} {:12.2f} {}'.format('ANOVA', i, len(replicas[i]), res_table_stat[i][0], res_table_stat[i][1]))
+		log_main.add('{:5} {:^7d} {:^8d} {:12.2f} {:12.2f}'.format('ANOVA', i, len(replicas[i]), res_table_stat[i][0], res_table_stat[i][1]))
 	log_main.add(' ')
 	log_main.add('ANOVA  Pair-wise tests')
-	log_main.add('{:5} {:^3} {:^3} {:^12} {:^12} {:^12} {:^12} {}'.format('ANOVA', 'A', 'B', 'avgA','avgB', 'P_value', 'f-value')) 
+	log_main.add('{:5} {:^3} {:^3} {:^12} {:^12} {:^12} {:^12}'.format('ANOVA', 'A', 'B', 'avgA','avgB', 'P_value', 'f-value')) 
 	for ires in xrange(K-1):
 		for jres in xrange(ires+1, K):
 			cres = stats.f_oneway(replicas[ires], replicas[jres])
-			log_main.add('{:5} {:^3d} {:^3d} {:12.4f} {:12.4f} {:12.3f} {:12.4f} {}'.format('ANOVA', ires, jres, avgs[ires], avgs[jres], cres[0], cres[1]))
+			log_main.add('{:5} {:^3d} {:^3d} {:12.4f} {:12.4f} {:12.3f} {:12.4f} '.format('ANOVA', ires, jres, avgs[ires], avgs[jres], cres[0], cres[1]))
 	log_main.add(' ')
 	log_main.add('================================================================================================================\n')
 
